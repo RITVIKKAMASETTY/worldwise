2a9e43504235dd618e4a5b093afecee6e3288ba5
RITVIKKAMASETTY <ritvikksymp@gmail.com>
2025-06-01 18:02:32 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
-import {useEffect,useState} from 'react';
-import HomePage from './pages/Homepage.jsx';
-import PageNotFound from './pages/PageNotFound.jsx';
-import Pricing from './pages/Pricing.jsx';
-import Product from './pages/Product.jsx';
-import Login from './pages/Login.jsx';
-import Apployout from './components/AppLayout.jsx';
+import {lazy, Suspense, useEffect,useState} from 'react';
 import Citylist from './components/Citylist.jsx';
 import Countrylist from './components/Countrylist.jsx';
 import City from './components/City.jsx';
@@ -13,12 +7,29 @@ import Form from './components/Form.jsx';
 import { CitiesProvider,useCities } from './components/context/CitiesContext.jsx';
 import {FakeAuth } from './components/context/fakeauth.jsx';
 import Protectedroute from './pages/Protectedroute.jsx';
+import Spinner from './components/Spinner.jsx';
+import SpinnerFullPage from './components/SpinnerFullPage.jsx';
+
+
+// import Login from './pages/Login.jsx';
+// import Apployout from './components/AppLayout.jsx';
+// import Pricing from './pages/Pricing.jsx';
+// import Product from './pages/Product.jsx';
+// import HomePage from './pages/Homepage.jsx';
+// import PageNotFound from './pages/PageNotFound.jsx';
+const HomePage=lazy(()=>import('./pages/Homepage.jsx'));
+const Login=lazy(()=>import('./pages/Login.jsx'));
+const Apployout=lazy(()=>import('./components/AppLayout.jsx'));
+const Pricing=lazy(()=>import('./pages/Pricing.jsx'));
+const Product=lazy(()=>import('./pages/Product.jsx'));
+const PageNotFound=lazy(()=>import('./pages/PageNotFound.jsx'));
 const baseurl="http://localhost:3001";
 function App() {
   
   return (<FakeAuth>
   <CitiesProvider>
     <BrowserRouter>
+    <Suspense fallback={<SpinnerFullPage/>}>
     <Routes>
       <Route index element={<HomePage />} />
 
@@ -37,6 +48,7 @@ function App() {
       <Route path="/login" element={<Login/>}/>
       <Route path="*" element={<PageNotFound />} />
     </Routes>
+    </Suspense>
   </BrowserRouter>
   </CitiesProvider>
   </FakeAuth>
